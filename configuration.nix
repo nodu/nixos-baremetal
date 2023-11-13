fcd2bbd0fc62d545de0dfc63195b9a066acda855
Matt Nodurfth <mnodurft@gmail.com>
2023-11-13 16:33:05 -0700

    Enable i3

--- configuration.nix
+++ configuration.nix
@@ -75,7 +75,9 @@ console.useXkbConfig = true;
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
+services.xserver.windowManager = {
+  i3.enable = true;
+};
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
