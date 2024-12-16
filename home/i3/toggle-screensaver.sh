68ce2df97a338060651b18af42afefa515714f81
Matt Nodurfth <mnodurft@gmail.com>
2024-12-16 11:41:16 -0800

    i3wm mods: i3blocks screensaver; modify screen shot keybindings

--- home/i3/toggle-screensaver.sh
+++ home/i3/toggle-screensaver.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 
+screen_saver_on=true
 dpms_state=$(xset q | grep -Po 'DPMS is \K(Enabled|Disabled)')
 if [ "$dpms_state" == "Enabled" ]; then
-	xset -dpms
-	dunstify "DPMS: Off"
+  xset -dpms
+  dunstify "DPMS: Off"
+  screen_saver_on=false
 else
-	xset +dpms #dpms 0 300 300
-	dunstify "DPMS: On"
+  xset +dpms #dpms 0 300 300
+  dunstify "DPMS: On"
+  screen_saver_on=true
 fi
 
 screensaver_state=$(xset q | grep -Po 'timeout:\s+\K(\d+)')
 if [ "$screensaver_state" -eq 0 ]; then
-	xset s 280 300 #on
-	dunstify "Screensaver: On"
+  xset s 280 300 #on
+  dunstify "Screensaver: On"
+  screen_saver_on=true
 else
-	xset s off
-	dunstify "Screensaver: Off"
+  xset s off
+  dunstify "Screensaver: Off"
+  screen_saver_on=false
 fi
+
+if [ $screen_saver_on == true ]; then
+  rtn=䷺
+else
+  rtn=❎
+fi
+
+echo "$rtn"
