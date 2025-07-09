87196ff6e03b655d8653e7f89f1483027c59fdb5
Matt Nodurfth <mnodurft@gmail.com>
2025-07-09 15:05:17 -0700

    Quotes

--- home/i3/monitor-disable.sh
+++ home/i3/monitor-disable.sh
@@ -2,6 +2,6 @@
 
 connected_monitor=$(xrandr | grep ' connected' | grep -v 'eDP\|LVDS' | awk '{ print $1 }')
 
-xrandr --output $connected_monitor --off
+xrandr --output "$connected_monitor" --off
 xrandr --output eDP-1 --primary
 dunstify "Secondary Monitor: Off"
